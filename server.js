1d70e1aaccdc88fb5901005d4ca7b47f0f29b6fd
ryanweiler92 <ryanweiler92@gmail.com>
2022-01-26 11:05:21 -0500

    troubleshoot heroku error

--- server.js
+++ server.js
@@ -41,5 +41,5 @@ app.get('/api/animals', (req, res) => {
 })
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}`);
+    console.log(`API server now on port ${PORT}!`);
 });
