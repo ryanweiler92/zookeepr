e143ff630047b99ce2b8aa631560982ee1a3278f
ryanweiler92 <ryanweiler92@gmail.com>
2022-01-26 13:05:04 -0500

    display results for multiple parameters

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { animals } = require('./data/animals');
-const express = require('express')
+const express = require('express');
+const e = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -31,6 +32,10 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -39,6 +44,15 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results)
 })
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+    res.json(result)
+    } else {
+        res.send(404)
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
